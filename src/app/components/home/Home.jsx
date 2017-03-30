583143e1b66d4af078bc5e6088b346c792393546
Rachel <racheldepriest1@gmail.com>
2017-03-30 15:45:45 -0500

    added more packages and updated index.js

--- src/app/components/home/Home.jsx
+++ src/app/components/home/Home.jsx
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 import AboutMe from '../aboutMe/AboutMe';
 import Introduction from '../introduction/Introduction';
 import Skills from '../skills/Skills';
+import NavBar from '../navBar/NavBar';
 
 export default () => (
-	<div></div>
+	<div id="page-wrap">
+		<NavBar />
+	</div>
 	)
---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, Switch } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+
+import AboutMe from './components/aboutMe/AboutMe';
+import Introduction from './components/introduction/Introduction';
+import Skills from './components/skills/Skills';
+import NavBar from './components/navBar/NavBar';
+import Home from './components/home/Home';
+
+
+ReactDOM.render(
+	)
